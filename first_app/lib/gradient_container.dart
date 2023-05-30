75d9b09d06d5abf9c1539fc8d006707b29260f74
Khaing Su Thiri <khaingsuthiri@gmail.com>
2023-05-30 09:03:50 +0630

    [Finished] Section 2 - Roll Dice App

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  // const GradientContainer.purple({super.key})
+  //     : color1 = Colors.deepPurple,
+  //       color2 = Colors.indigo;
+
+  final List<Color> colors;
+  // final Color color1;
+  // final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
